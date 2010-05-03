3cc4d555bb7dc8a8c7bc9a19413e45184362d083
Kevin Lee <kevin@elixirian.com>
2010-05-03 05:27:35 +0000

    Complete - # 14: Create common constants https://trac.elixirian.com/elixirian-common/ticket/14

---
+++ src/main/java/com/elixirian/common/util/CommonConstants.java
@@ -0,0 +1,21 @@
+package com.elixirian.common.util;
+
+/**
+ * This is a utility class, which has common constants.
+ * 
+ * @author Lee, SeongHyun (Kevin)
+ * @version 0.0.1 (2010-03-07)
+ */
+public final class CommonConstants
+{
+	/**
+	 * " cannot be instantiated."
+	 */
+	public static final String CANNOT_BE_INSTANTIATED = " cannot be instantiated.";
+
+	private CommonConstants()
+	{
+		throw new IllegalStateException(getClass().getName() + CANNOT_BE_INSTANTIATED);
+	}
+
+}
