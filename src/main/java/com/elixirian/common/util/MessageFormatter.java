f0b9abb50d7d723b591a3d4459094bc8c397018d
Kevin Lee <kevin@elixirian.com>
2010-05-04 02:05:01 +0000

    ## 0.0.2-SNAPSHOT Complete - # 18: Recreate MessageFormatter https://trac.elixirian.com/elixirian-common/ticket/18

---
+++ src/main/java/com/elixirian/common/util/MessageFormatter.java
@@ -0,0 +1,176 @@
+package com.elixirian.common.util;
+
+/**
+ * @author Lee, SeongHyun (Kevin)
+ * @version 0.0.1 (2010-03-02)
+ * @version 0.0.2 (2010-05-04) the {@link #format(String, Object...)} method is recreated (moved from {@link Strings}.
+ */
+public final class MessageFormatter
+{
+	/**
+	 * '%'
+	 */
+	public static final char ESCAPE_CHAR = '%';
+
+	/**
+	 * "%s"
+	 */
+	public static final String PLACE_HOLDER_SYMBOL = ESCAPE_CHAR + "s";
+
+	/**
+	 * 2
+	 */
+	public static final int JUMP_SIZE = 2;
+
+	private MessageFormatter()
+	{
+		throw new IllegalStateException(getClass().getName() + CommonConstants.CANNOT_BE_INSTANTIATED);
+	}
+
+	/**
+	 * Format the message with the given argument objects. The place holder is
+	 * <code>%s<code>. all the "%s"s are replaced by the given argument objects.
+	 * <ul>
+	 * <li>If there are more argument objects passed, the rest are added the end of the message in the square brackets.</li> 
+	 * <li>If there are fewer "%s"s entered, the rest are removed from the message.</li>
+	 * <li>To include <code>%s</code> in the message, put <code>%</code> just before <code>%s</code> (e.g. %%s / the first % is escape
+	 * character).</li> </ul> <div> e.g.) </div>
+	 * 
+	 * <pre>
+	 * Message:   "Hello, %s. How are you %s?"
+	 * Arguments: "world", "today"
+	 * Result:    "Hello, world. How are you today?"
+	 * </pre>
+	 * 
+	 * <pre>
+	 * Message:   "Hello, %s. How are you %s?"
+	 * Arguments: "world", "today", "Kevin", "Lee"
+	 * Result:    "Hello, world. How are you today? [Kevin, Lee]"
+	 * </pre>
+	 * 
+	 * <pre>
+	 * Message:   "Hello, %s. How are you %s? So you have more %s. Oh! another %s?"
+	 * Arguments: "world", "today"
+	 * Result:    "Hello, world. How are you today? So you have more . Oh! another ?"
+	 * </pre>
+	 * 
+	 * <pre>
+	 * Message:   "Hello, %s. How are you %s? %%s is escaped, but this %s is not."
+	 * Arguments: "world", "today", "place holder"
+	 * Result:    "Hello, world. How are you today? %s is escaped, but this place holder is not."
+	 * </pre>
+	 * 
+	 * This is correct.
+	 * 
+	 * <pre>
+	 * format(&quot;some message&quot;, (Object) null);
+	 * </pre>
+	 * 
+	 * but the following code is not. This results in {@link NullPointerException}.
+	 * 
+	 * <pre>
+	 * format(&quot;some message&quot;, null);
+	 * </pre>
+	 * 
+	 * If there is no argument object then just do not pass anything.
+	 * 
+	 * <pre>
+	 * format(&quot;some message&quot;);
+	 * </pre>
+	 * 
+	 * To sum up,
+	 * 
+	 * <pre>
+	 * format(&quot;some message&quot;, null); // WRONG!
+	 * </pre>
+	 * 
+	 * <pre>
+	 * format(&quot;some message&quot;); // FINE
+	 * </pre>
+	 * 
+	 * <pre>
+	 * format(&quot;some message&quot;, (Object) null); // FINE 
+	 * // but no point to do this unless the given object variable contains null reference like the following code.
+	 * 
+	 * // somehow object contains null.
+	 * format(&quot;some %s message&quot;, object);
+	 * 
+	 * // result is
+	 * &quot;some null message&quot;
+	 * </pre>
+	 * 
+	 * @param message
+	 *            the given message.
+	 * @param args
+	 *            the given argument objects.
+	 * @return formatted message.
+	 */
+	public static String format(final String message, final Object... args)
+	{
+		int i = 0;
+		int fromIndex = 0;
+		final StringBuilder formattedMessage = new StringBuilder();
+		while (args.length > i)
+		{
+			int position = message.indexOf(PLACE_HOLDER_SYMBOL, fromIndex);
+
+			if (0 > position)
+			{
+				break;
+			}
+			if (0 < position && message.charAt(position - 1) == ESCAPE_CHAR)
+			{
+				/* "%%s" is found so escape. */
+				formattedMessage.append(message.substring(fromIndex, position))
+						.append(message.charAt(position + 1));
+			}
+			else
+			{
+				/* "%s" is found so replace it with one of the arguments. */
+				formattedMessage.append(message.substring(fromIndex, position))
+						.append(args[i++]);
+			}
+			fromIndex = position + JUMP_SIZE;
+		}
+
+		int position = message.indexOf(PLACE_HOLDER_SYMBOL, fromIndex);
+		if (0 == position)
+		{
+			/* 0 == position means it can never be "%%s" so treat it separately then testing the position against 0 is not required anymore. */
+			fromIndex = position + JUMP_SIZE;
+			position = message.indexOf(PLACE_HOLDER_SYMBOL, fromIndex);
+		}
+		while (0 < position)
+		{
+			/* remove all the extra "%s"s and escape the rest of the "%%s"s */
+			if (message.charAt(position - 1) == ESCAPE_CHAR)
+			{
+				/* "%%s" is found so escape then jump to the next position. */
+				formattedMessage.append(message.substring(fromIndex, position))
+						.append(message.charAt(position + 1));
+			}
+			else
+			{
+				/* "%s" is found so take all the Strings before it then jump to the next postion. */
+				formattedMessage.append(message.substring(fromIndex, position));
+			}
+			fromIndex = position + JUMP_SIZE;
+			position = message.indexOf(PLACE_HOLDER_SYMBOL, fromIndex);
+		}
+		formattedMessage.append(message.substring(fromIndex));
+
+		if (args.length > i)
+		{
+			/* put all the extra arguments to the end of the message. */
+			formattedMessage.append(0 < formattedMessage.length() ? " [" : "[")
+					.append(args[i++]);
+			while (args.length > i)
+			{
+				formattedMessage.append(", ")
+						.append(args[i++]);
+			}
+			formattedMessage.append("]");
+		}
+		return formattedMessage.toString();
+	}
+}
---
+++ src/test/java/com/elixirian/common/util/MessageFormatterTest.java
@@ -0,0 +1,167 @@
+/**
+ * 
+ */
+package com.elixirian.common.util;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Lee, SeongHyun (Kevin)
+ * @version 0.0.1 (2010-05-04)
+ */
+public class MessageFormatterTest
+{
+	private static class Person
+	{
+		private final String surname;
+		private final String givenName;
+
+		public Person(String surname, String givenName)
+		{
+			this.surname = surname;
+			this.givenName = givenName;
+		}
+
+		@Override
+		public String toString()
+		{
+			return givenName + " " + surname;
+		}
+	}
+
+	/**
+	 * Test method for {@link com.elixirian.common.util.MessageFormatter#format(String, Object...)}.
+	 */
+	@Test
+	public final void testFormat()
+	{
+		String result = MessageFormatter.format("Hello, %s", "World");
+		System.out.println("Message:  \"Hello, %s\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %s.", "World");
+		System.out.println("Message:  \"Hello, %s.\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World."));
+		System.out.println("===================================================");
+
+		result =
+			MessageFormatter.format("Hello, %s. My name is %s. This %s is to check if %s works well.", "World", new Person("Lee", "Kevin"),
+					"test", MessageFormatter.class);
+		System.out.println("Message:  \"Hello, %s. My name is %s. This %s is to check if %s works well.\""
+				+ "\nArgument: \"World\", new Person(\"Lee\", \"Kevin\"), \"test\", Strings.class" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World. My name is Kevin Lee. This test is to check if " + MessageFormatter.class.toString()
+				+ " works well."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %%s%s", "World");
+		System.out.println("Message:  \"Hello, %%s%s\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, %sWorld"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %s. How are you %s?", "World", "today");
+		System.out.println("Message:  \"Hello, %s. How are you %s?\"" + "\nArgument: \"World\", \"today\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World. How are you today?"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %%%s%s%s.", "World");
+		System.out.println("Message:  \"Hello, %%%s%s%s.\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, %%sWorld."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %%%s%s%s.%s", "World");
+		System.out.println("Message:  \"Hello, %%%s%s%s.%s\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, %%sWorld."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %%%s%s%%s.", "World");
+		System.out.println("Message:  \"Hello, %%%s%s%%s.\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, %%sWorld%s."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %%%s%%s%s.", "World");
+		System.out.println("Message:  \"Hello, %%%s%%s%s.\"" + "\nArgument: \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, %%s%sWorld."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %s. How are you %s?", "World", "today", "sender", "is", new Person("Lee", "Kevin"));
+		System.out.println("Message:  \"Hello, %s. How are you %s?\""
+				+ "\nArgument: \"World\", \"today\", \"sender\", \"is\", new Person(\"Lee\", \"Kevin\")" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World. How are you today? [sender, is, Kevin Lee]"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("", "Hello", "World");
+		System.out.println("Message:  \"\"" + "\nArgument: \"Hello\", \"World\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("[Hello, World]"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, World");
+		System.out.println("Message:  \"Hello, World\"" + "\nArgument: " + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, [%s]. I am [%s].");
+		System.out.println("Message:  \"Hello, [%s]. I am [%s].\"" + "\nArgument: " + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, []. I am []."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s", "test");
+		System.out.println("Message:  \"%s\"" + "\nArgument: \"test\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("test"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s", "test", "another");
+		System.out.println("Message:  \"%s\"" + "\nArgument: \"test\", \"another\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("test [another]"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s %s.", "test");
+		System.out.println("Message:  \"%s %s.\"" + "\nArgument: \"test\"" + "\nResult:   " + result);
+		assertThat(result, equalTo("test ."));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s");
+		System.out.println("Message:  \"%s\"" + "\nArgument: " + "\nResult:   " + result);
+		assertThat(result, equalTo(""));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s%s%s");
+		System.out.println("Message:  \"%s%s%s\"" + "\nArgument: " + "\nResult:   " + result);
+		assertThat(result, equalTo(""));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s%%s%s%%s");
+		System.out.println("Message:  \"%s%%s%s%%s\"" + "\nArgument: " + "\nResult:   " + result);
+		assertThat(result, equalTo("%s%s"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("%s", (Object) null);
+		System.out.println("Message:  \"%s\"" + "\nArgument: (Object) null" + "\nResult:   " + result);
+		assertThat(result, equalTo("null"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, World", (Object) null);
+		System.out.println("Message:  \"Hello, World\"" + "\nArgument: (Object) null" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, World [null]"));
+		System.out.println("===================================================");
+
+		result = MessageFormatter.format("Hello, %s", (Object) null);
+		System.out.println("Message:  \"Hello, %s\"" + "\nArgument: (Object) null" + "\nResult:   " + result);
+		assertThat(result, equalTo("Hello, null"));
+		System.out.println("===================================================");
+
+	}
+
+	/**
+	 * Test method for {@link com.elixirian.common.util.MessageFormatter#format(String, Object...)}.
+	 */
+	@Test(expected = NullPointerException.class)
+	public final void testFormatWithInvalidArgument()
+	{
+		MessageFormatter.format("Hello, World", null);
+	}
+
+}
