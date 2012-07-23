988abd8b079842ce7164a878a5f124c513f83940
Kevin Lee <kevin@elixirian.com>
2012-07-23 14:00:55 +0000

    ## 0.0.10-SNAPSHOT (ready for deployment) *Added -ParamInfo type

---
+++ src/main/java/org/elixirian/kommonlee/type/ParamInfo.java
@@ -0,0 +1,272 @@
+/**
+ * 
+ */
+package org.elixirian.kommonlee.type;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.elixirian.kommonlee.type.checkable.EmptinessCheckable;
+
+/**
+ * <pre>
+ *     ___  _____                                              _____
+ *    /   \/    / ______ __________________  ______ __ ______ /    /   ______  ______  
+ *   /        / _/ __  // /  /   / /  /   /_/ __  // //     //    /   /  ___ \/  ___ \ 
+ *  /        \ /  /_/ _/  _  _  /  _  _  //  /_/ _/   __   //    /___/  _____/  _____/
+ * /____/\____\/_____//__//_//_/__//_//_/ /_____//___/ /__//________/\_____/ \_____/
+ * </pre>
+ * 
+ * <pre>
+ *     ___  _____                                _____
+ *    /   \/    /_________  ___ ____ __ ______  /    /   ______  ______
+ *   /        / /  ___ \  \/  //___// //     / /    /   /  ___ \/  ___ \
+ *  /        \ /  _____/\    //   //   __   / /    /___/  _____/  _____/
+ * /____/\____\\_____/   \__//___//___/ /__/ /________/\_____/ \_____/
+ * </pre>
+ * 
+ * @author Lee, SeongHyun (Kevin)
+ * @version 0.0.1 (2012-06-05)
+ */
+public class ParamInfo<T> implements EmptinessCheckable
+{
+	public static final List<ParamInfo<?>> EMPTY_PARAM_LIST = Collections.emptyList();
+
+	public static final List<Class<? extends Annotation>> EMPTY_ANNOTATION_LIST = Collections.emptyList();
+
+	public static final ParamInfo<?> EMPTY_PARAM_INFO =
+		new ParamInfo<Object>(null, "", null, EMPTY_ANNOTATION_LIST, null) {
+			@Override
+			public int hashCode()
+			{
+				return 0;
+			}
+
+			@Override
+			public boolean isEmpty()
+			{
+				return true;
+			}
+		};
+
+	private final Class<T> paramType;
+
+	private final String paramName;
+
+	private final T paramValue;
+
+	private final List<Class<? extends Annotation>> paramAnnotationList;
+
+	private final String note;
+
+	public static class Builder<T> implements GenericBuilder<ParamInfo<T>>
+	{
+		final Class<T> paramType;
+
+		final String paramName;
+
+		T paramValue;
+
+		final List<Class<? extends Annotation>> paramAnnotationList;
+
+		String note;
+
+		public Builder(final Class<T> paramType, final String paramName)
+		{
+			this.paramType = paramType;
+			this.paramName = paramName;
+			this.paramAnnotationList = new ArrayList<Class<? extends Annotation>>();
+		}
+
+		public Builder<T> paramValue(final T paramValue)
+		{
+			this.paramValue = paramValue;
+			return this;
+		}
+
+		public <A extends Annotation> Builder<T> addParamAnnotation(final Class<A> annotation)
+		{
+			this.paramAnnotationList.add(annotation);
+			return this;
+		}
+
+		public <A extends Annotation> Builder<T> addAllParamAnnotation(final Class<? extends A>... annotation)
+		{
+			this.paramAnnotationList.addAll(Arrays.asList(annotation));
+			return this;
+		}
+
+		public Builder<T> note(final String note)
+		{
+			this.note = note;
+			return this;
+		}
+
+		@Override
+		public ParamInfo<T> build()
+		{
+			/* @formatter:off */
+      if (null == paramType &&
+          (null == paramName ||
+          	paramName.isEmpty()) &&
+          null == paramValue &&
+          paramAnnotationList.isEmpty() &&
+          (null == note ||
+          	note.isEmpty()))
+      {
+        /* @formatter:on */
+				@SuppressWarnings("unchecked")
+				final ParamInfo<T> emptyParamInfo = (ParamInfo<T>) EMPTY_PARAM_INFO;
+				return emptyParamInfo;
+			}
+			return new ParamInfo<T>(this);
+		}
+	}
+
+	public static <T> Builder<T> builder(final Class<T> paramType, final String paramName)
+	{
+		return new Builder<T>(paramType, paramName);
+	}
+
+	private ParamInfo(final Builder<T> builder)
+	{
+		this(builder.paramType, builder.paramName, builder.paramValue, builder.paramAnnotationList, builder.note);
+	}
+
+	private ParamInfo(final Class<T> paramType, final String paramName, final T paramValue,
+			final List<Class<? extends Annotation>> paramAnnotationList, final String note)
+	{
+		this.paramType = paramType;
+		this.paramName = paramName;
+		this.paramValue = paramValue;
+		/* @formatter:off */
+      this.paramAnnotationList =
+        null == paramAnnotationList || paramAnnotationList.isEmpty() ?
+            EMPTY_ANNOTATION_LIST :
+            Collections.unmodifiableList(paramAnnotationList);
+    /* @formatter:on */
+		this.note = note;
+	}
+
+	/**
+	 * Returns the paramType.
+	 * 
+	 * @return the type ({@link Class}) of the parameter.
+	 */
+	public Class<?> getParamType()
+	{
+		return paramType;
+	}
+
+	/**
+	 * Returns paramName
+	 * 
+	 * @return the name of the parameter.
+	 */
+	public String getParamName()
+	{
+		return paramName;
+	}
+
+	/**
+	 * Returns paramValue.
+	 * 
+	 * @return the parameter value.
+	 */
+	public T getParamValue()
+	{
+		return paramValue;
+	}
+
+	/**
+	 * Returns paramAnnotationList.
+	 * 
+	 * @return the {@link List} of {@link Class} that extends {@link Annotation} with which the parameter is annotated.
+	 */
+	public List<Class<? extends Annotation>> getParamAnnotationList()
+	{
+		return paramAnnotationList;
+	}
+
+	public String getNote()
+	{
+		return note;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int result = 1;
+		result = 31 * result + (null == paramName ? 0 : paramName.hashCode());
+		result = 31 * result + (null == paramType ? 0 : paramType.hashCode());
+		result = 31 * result + (null == paramValue ? 0 : paramValue.hashCode());
+		result = 31 * result + (null == paramAnnotationList ? 0 : paramAnnotationList.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object paramInfo)
+	{
+		if (this == paramInfo)
+		{
+			return true;
+		}
+		if (!(paramInfo instanceof ParamInfo))
+		{
+			return false;
+		}
+		final ParamInfo<?> that = (ParamInfo<?>) paramInfo;
+		/* @formatter:off */
+    return (this.paramName == that.getParamName() ||
+            	(null != this.paramName &&
+            	 this.paramName.equals(that.getParamName()))) &&
+           (this.paramType == that.getParamType() ||
+            	(null != this.paramType &&
+        			 this.paramType.equals(that.getParamType()))) &&
+           (this.paramValue == that.getParamValue() ||
+              (null != this.paramValue &&
+          		 this.paramValue.equals(that.getParamValue()))) &&
+           (this.paramAnnotationList == that.getParamAnnotationList() ||
+            	(null != this.paramAnnotationList &&
+        			this.paramAnnotationList.equals(that.getParamAnnotationList())));
+    /* @formatter:on */
+	}
+
+	@Override
+	public String toString()
+	{
+		if (isEmpty())
+		{
+			return "{EMPTY}";
+		}
+		/* @formatter:off */
+    return new StringBuilder(this.getClass().getSimpleName())
+    		.append("{")
+	        .append("paramName=")
+	        .append(paramName)
+        .append(", ")
+	        .append("paramType=")
+	        .append(paramType)
+        .append(", ")
+	        .append("paramValue=")
+	        .append(paramValue)
+        .append(", ")
+	        .append("paramAnnotationList=")
+	        .append(paramAnnotationList)
+        .append(", ")
+	        .append("note=")
+	        .append(note)
+        .append("}")
+        .toString();
+    /* @formatter:on */
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		return EMPTY_PARAM_INFO.equals(this);
+	}
+}
---
+++ src/test/java/org/elixirian/kommonlee/type/ParamInfoTest.java
@@ -0,0 +1,340 @@
+package org.elixirian.kommonlee.type;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ParamInfoTest
+{
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+	}
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Documented
+	@Target(ElementType.PARAMETER)
+	@Retention(RetentionPolicy.RUNTIME)
+	static @interface TestParam
+	{
+		String value() default "Test";
+	}
+
+	@Documented
+	@Target(ElementType.PARAMETER)
+	@Retention(RetentionPolicy.RUNTIME)
+	static @interface AnotherTestParam
+	{
+		String value() default "Another Test";
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public final void testHashCode()
+	{
+		/* given */
+		final ParamInfo<String> expectedParamInfo = ParamInfo.builder(String.class, "testParam")
+				.paramValue("Kevin")
+				.addParamAnnotation(TestParam.class)
+				.addParamAnnotation(AnotherTestParam.class)
+				.build();
+
+		final int expected = expectedParamInfo.hashCode();
+
+		/* when */
+		final ParamInfo<String> actual = ParamInfo.builder(String.class, "testParam")
+				.paramValue("Kevin")
+				.addParamAnnotation(TestParam.class)
+				.addParamAnnotation(AnotherTestParam.class)
+				.build();
+
+		/* then */
+		assertThat(actual.hashCode(), is(equalTo(expected)));
+
+		/* given */
+		final int expected2 = ParamInfo.EMPTY_PARAM_INFO.hashCode();
+
+		/* when */
+		final ParamInfo<?> actual2 = ParamInfo.builder(null, null)
+				.build();
+
+		/* then */
+		assertThat(actual2.hashCode(), is(equalTo(expected2)));
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public final void testHashCode1()
+	{
+		/* given */
+		final int expected = ParamInfo.EMPTY_PARAM_INFO.hashCode();
+
+		/* when */
+		final ParamInfo<?> actual = ParamInfo.builder(null, null)
+				.build();
+
+		/* then */
+		assertThat(actual.hashCode(), is(equalTo(expected)));
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public final void testHashCode2()
+	{
+		/* given */
+		final ParamInfo<String> expectedParamInfo = ParamInfo.builder(String.class, "testParam")
+				.paramValue("Kevin")
+				.addParamAnnotation(TestParam.class)
+				.build();
+
+		final int expected = expectedParamInfo.hashCode();
+
+		/* when */
+		final ParamInfo<String> actual = ParamInfo.builder(String.class, "testParam")
+				.paramValue("Kevin")
+				.addParamAnnotation(AnotherTestParam.class)
+				.build();
+
+		/* then */
+		assertThat(actual.hashCode(), is(not(equalTo(expected))));
+	}
+
+	@Test
+	public final void testGetParamType()
+	{
+		/* given */
+		@SuppressWarnings("rawtypes")
+		final Class expected = String.class;
+
+		final ParamInfo<String> paramInfo = ParamInfo.builder(String.class, "testParam")
+				.paramValue("Kevin")
+				.addParamAnnotation(TestParam.class)
+				.addParamAnnotation(AnotherTestParam.class)
+				.build();
+
+		/* when */
+		@SuppressWarnings("rawtypes")
+		final Class actual = paramInfo.getParamType();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@Test
+	public final void testGetParamType1()
+	{
+		/* given */
+		@SuppressWarnings("rawtypes")
+		final Class expected = null;
+
+		final ParamInfo<?> paramInfo = ParamInfo.builder(null, null)
+				.build();
+
+		/* when */
+		@SuppressWarnings("rawtypes")
+		final Class actual = paramInfo.getParamType();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+		assertThat(actual, is(nullValue()));
+	}
+
+	@Test
+	public final void testGetParamName()
+	{
+		/* given */
+		final String expected = "supplierName";
+		final ParamInfo<String> paramInfo = ParamInfo.builder(String.class, expected)
+				.build();
+
+		/* when */
+		final String actual = paramInfo.getParamName();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@Test
+	public final void testGetParamValue()
+	{
+		/* given */
+		final String expected = "Kevin Lee";
+		final ParamInfo<String> paramInfo = ParamInfo.builder(String.class, "name")
+				.paramValue(expected)
+				.build();
+
+		/* when */
+		final String actual = paramInfo.getParamValue();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@Test
+	public final void testGetParamAnnotationList()
+	{
+		/* given */
+		@SuppressWarnings("unchecked")
+		final List<Class<? extends Annotation>> expected = Arrays.asList(TestParam.class, AnotherTestParam.class);
+
+		final ParamInfo.Builder<Long> paramInfoBuilder = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L));
+
+		for (final Class<? extends Annotation> annotationClass : expected)
+		{
+			paramInfoBuilder.addParamAnnotation(annotationClass);
+		}
+		final ParamInfo<Long> paramInfo = paramInfoBuilder.build();
+
+		/* when */
+		final List<Class<? extends Annotation>> actual = paramInfo.getParamAnnotationList();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@Test
+	public final void testGetNote()
+	{
+		/* given */
+		final String expected = "Please read it carefully.";
+		final ParamInfo<Long> paramInfo = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L))
+				.note(expected)
+				.build();
+
+		/* when */
+		final String actual = paramInfo.getNote();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public final void testEqualsObject()
+	{
+		/* given */
+		final boolean expected = true;
+		final ParamInfo<Long> paramInfo1 = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L))
+				.build();
+		final ParamInfo<Long> paramInfo2 = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L))
+				.build();
+
+		/* when */
+		final boolean actual = paramInfo1.equals(paramInfo2);
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public final void testEqualsObject1()
+	{
+		/* given */
+		final boolean expected = false;
+		final ParamInfo<Long> paramInfo1 = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(999L))
+				.build();
+		final ParamInfo<Long> paramInfo2 = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L))
+				.build();
+
+		/* when */
+		final boolean actual = paramInfo1.equals(paramInfo2);
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@SuppressWarnings("boxing")
+	@Test
+	public final void testEqualsObject2()
+	{
+		/* given */
+		final boolean expected = true;
+		final ParamInfo<Long> paramInfo1 = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L))
+				.note("Please read it carefully.")
+				.build();
+		final ParamInfo<Long> paramInfo2 = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(100L))
+				.note("another note")
+				.build();
+
+		/* when */
+		final boolean actual = paramInfo1.equals(paramInfo2);
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@Test
+	public final void testToString()
+	{
+		/* given */
+		final String expected =
+			"ParamInfo{paramName=id, paramType=class java.lang.Long, paramValue=999, paramAnnotationList=[interface org.elixirian.kommonlee.type.ParamInfoTest$TestParam, interface org.elixirian.kommonlee.type.ParamInfoTest$AnotherTestParam], note=Please read it carefully.}";
+		@SuppressWarnings("unchecked")
+		final ParamInfo<Long> paramInfo = ParamInfo.builder(Long.class, "id")
+				.paramValue(Long.valueOf(999L))
+				.addAllParamAnnotation(TestParam.class, AnotherTestParam.class)
+				.note("Please read it carefully.")
+				.build();
+
+		/* when */
+		final String actual = paramInfo.toString();
+
+		/* then */
+		assertThat(actual, is(equalTo(expected)));
+	}
+
+	@SuppressWarnings({ "boxing", "rawtypes" })
+	@Test
+	public final void testIsEmpty()
+	{
+		/* given */
+		final Boolean expected = Boolean.TRUE;
+
+		/* when */
+		final ParamInfo<?> actual = ParamInfo.builder(null, null)
+				.build();
+
+		/* then */
+		assertThat(actual.isEmpty(), is(equalTo(expected)));
+		assertThat(actual, is(equalTo(((ParamInfo) ParamInfo.EMPTY_PARAM_INFO))));
+	}
+
+}
