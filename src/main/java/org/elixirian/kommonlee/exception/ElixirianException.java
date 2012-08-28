b8baa6ae13d939ff1b714be8f8dc844ad35f4ab5
Kevin Lee <kevin@elixirian.com>
2012-08-29 05:30:39 +1000

    ## 0.0.11-SNAPSHOT (ready for deployment) *Changed -Refactored...

--- src/main/java/org/elixirian/kommonlee/exception/ElixirianException.java
+++ src/main/java/org/elixirian/kommonlee/exception/ElixirianException.java
@@ -1,25 +1,53 @@
 /**
- * 
+ * This project is licensed under the Apache License, Version 2.0
+ * if the following condition is met:
+ * (otherwise it cannot be used by anyone but the author, Kevin, only)
+ *
+ * The original KommonLee project is owned by Lee, Seong Hyun (Kevin).
+ *
+ * -What does it mean to you?
+ * Nothing, unless you want to take the ownership of
+ * "the original project" (not yours or forked & modified one).
+ * You are free to use it for both non-commercial and commercial projects
+ * and free to modify it as the Apache License allows.
+ *
+ * -So why is this condition necessary?
+ * It is only to protect the original project (See the case of Java).
+ *
+ *
+ * Copyright 2009 Lee, Seong Hyun (Kevin)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.elixirian.kommonlee.exception;
 
 /**
  * <pre>
- *     ____________    ___________  ____   _______ _________ _______ _______________  ____
- *    /       /   /   /_    _/\   \/   /  /_    _//  __    //_    _//   __    /     \/   /
- *   /    ___/   /     /   /   \      /    /   / /  /_/   /  /   / /   /_/   /          /
- *  /    ___/   /_____/   /_   /      \  _/   /_/       _/ _/   /_/   __    /          /
- * /_______/________/______/  /___/\___\/______/___/\___\ /______/___/ /___/___/\_____/
+ *     ___  _____                                              _____
+ *    /   \/    / ______ __________________  ______ __ ______ /    /   ______  ______
+ *   /        / _/ __  // /  /   / /  /   /_/ __  // //     //    /   /  ___ \/  ___ \
+ *  /        \ /  /_/ _/  _  _  /  _  _  //  /_/ _/   __   //    /___/  _____/  _____/
+ * /____/\____\/_____//__//_//_/__//_//_/ /_____//___/ /__//________/\_____/ \_____/
  * </pre>
- * 
+ *
  * <pre>
- *     ___  _____  __________  ___________ _____  ____
- *    /   \/    / /      \   \/   /_    _//     \/   /
- *   /        /  /    ___/\      / /   / /          /
- *  /        \  /    ___/  \    /_/   /_/          /
- * /____/\____\/_______/    \__//______/___/\_____/
+ *     ___  _____                                _____
+ *    /   \/    /_________  ___ ____ __ ______  /    /   ______  ______
+ *   /        / /  ___ \  \/  //___// //     / /    /   /  ___ \/  ___ \
+ *  /        \ /  _____/\    //   //   __   / /    /___/  _____/  _____/
+ * /____/\____\\_____/   \__//___//___/ /__/ /________/\_____/ \_____/
  * </pre>
- * 
+ *
  * @author Lee, SeongHyun (Kevin)
  * @version 0.0.1 (2010-01-09)
  */
@@ -32,17 +60,17 @@ public class ElixirianException extends Exception
 		super();
 	}
 
-	public ElixirianException(String message)
+	public ElixirianException(final String message)
 	{
 		super(message);
 	}
 
-	public ElixirianException(String message, Throwable cause)
+	public ElixirianException(final String message, final Throwable cause)
 	{
 		super(message, cause);
 	}
 
-	public ElixirianException(Throwable cause)
+	public ElixirianException(final Throwable cause)
 	{
 		super(cause);
 	}
--- src/main/java/org/elixirian/kommonlee/exception/InformativeRuntimeException.java
+++ src/main/java/org/elixirian/kommonlee/exception/InformativeRuntimeException.java
@@ -1,17 +1,45 @@
 /**
- * 
+ * This project is licensed under the Apache License, Version 2.0
+ * if the following condition is met:
+ * (otherwise it cannot be used by anyone but the author, Kevin, only)
+ *
+ * The original KommonLee project is owned by Lee, Seong Hyun (Kevin).
+ *
+ * -What does it mean to you?
+ * Nothing, unless you want to take the ownership of
+ * "the original project" (not yours or forked & modified one).
+ * You are free to use it for both non-commercial and commercial projects
+ * and free to modify it as the Apache License allows.
+ *
+ * -So why is this condition necessary?
+ * It is only to protect the original project (See the case of Java).
+ *
+ *
+ * Copyright 2009 Lee, Seong Hyun (Kevin)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.elixirian.kommonlee.exception;
 
 /**
  * <pre>
  *     ___  _____                                              _____
- *    /   \/    / ______ __________________  ______ __ ______ /    /   ______  ______  
- *   /        / _/ __  // /  /   / /  /   /_/ __  // //     //    /   /  ___ \/  ___ \ 
+ *    /   \/    / ______ __________________  ______ __ ______ /    /   ______  ______
+ *   /        / _/ __  // /  /   / /  /   /_/ __  // //     //    /   /  ___ \/  ___ \
  *  /        \ /  /_/ _/  _  _  /  _  _  //  /_/ _/   __   //    /___/  _____/  _____/
  * /____/\____\/_____//__//_//_/__//_//_/ /_____//___/ /__//________/\_____/ \_____/
  * </pre>
- * 
+ *
  * <pre>
  *     ___  _____                                _____
  *    /   \/    /_________  ___ ____ __ ______  /    /   ______  ______
@@ -19,7 +47,7 @@ package org.elixirian.kommonlee.exception;
  *  /        \ /  _____/\    //   //   __   / /    /___/  _____/  _____/
  * /____/\____\\_____/   \__//___//___/ /__/ /________/\_____/ \_____/
  * </pre>
- * 
+ *
  * @author Lee, SeongHyun (Kevin)
  * @version 0.0.1 (2012-06-05)
  */
@@ -31,32 +59,43 @@ public abstract class InformativeRuntimeException extends ElixirianRuntimeExcept
 
 	public InformativeRuntimeException(final String message)
 	{
-		this(message, null, null);
+		super(message);
+		this.extraExceptionInformation = ExtraExceptionInformation.EMPTY_EXTRA_EXCEPTION_INFORMATION;
 	}
 
 	public InformativeRuntimeException(final Throwable cause)
 	{
-		this(null, cause, null);
+		super(cause);
+		this.extraExceptionInformation = ExtraExceptionInformation.EMPTY_EXTRA_EXCEPTION_INFORMATION;
 	}
 
 	public InformativeRuntimeException(final String message, final Throwable cause)
 	{
-		this(message, cause, null);
+		super(message, cause);
+		this.extraExceptionInformation = ExtraExceptionInformation.EMPTY_EXTRA_EXCEPTION_INFORMATION;
 	}
 
 	public InformativeRuntimeException(final ExtraExceptionInformation extraExceptionInformation)
 	{
-		this(null, null, extraExceptionInformation);
+		this.extraExceptionInformation =
+			null == extraExceptionInformation ? ExtraExceptionInformation.EMPTY_EXTRA_EXCEPTION_INFORMATION
+					: extraExceptionInformation;
 	}
 
 	public InformativeRuntimeException(final String message, final ExtraExceptionInformation extraExceptionInformation)
 	{
-		this(message, null, extraExceptionInformation);
+		super(message);
+		this.extraExceptionInformation =
+			null == extraExceptionInformation ? ExtraExceptionInformation.EMPTY_EXTRA_EXCEPTION_INFORMATION
+					: extraExceptionInformation;
 	}
 
 	public InformativeRuntimeException(final Throwable cause, final ExtraExceptionInformation extraExceptionInformation)
 	{
-		this(null, cause, extraExceptionInformation);
+		super(cause);
+		this.extraExceptionInformation =
+			null == extraExceptionInformation ? ExtraExceptionInformation.EMPTY_EXTRA_EXCEPTION_INFORMATION
+					: extraExceptionInformation;
 	}
 
 	public InformativeRuntimeException(final String message, final Throwable cause,
@@ -68,9 +107,14 @@ public abstract class InformativeRuntimeException extends ElixirianRuntimeExcept
 					: extraExceptionInformation;
 	}
 
-	public ExtraExceptionInformation getExceptionInformation()
+	public ExtraExceptionInformation getExtraExceptionInformation()
 	{
 		return extraExceptionInformation;
 	}
 
+	@Override
+	public String toString()
+	{
+		return super.toString() + "\n" + getExtraExceptionInformation();
+	}
 }
