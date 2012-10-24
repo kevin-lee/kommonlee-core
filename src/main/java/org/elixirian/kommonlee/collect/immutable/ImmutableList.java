66754849209a069d190d2003d1d82817a5cd0092
Kevin Lee <kevin@elixirian.com>
2012-10-24 17:47:33 +1100

    ## 0.0.12-SNAPSHOT (ready for deployment) -convertTo methods ReadableList and ImmutableList return List (it was Collection before).

--- src/main/java/org/elixirian/kommonlee/collect/immutable/ImmutableList.java
+++ src/main/java/org/elixirian/kommonlee/collect/immutable/ImmutableList.java
@@ -31,6 +31,8 @@
  */
 package org.elixirian.kommonlee.collect.immutable;
 
+import java.util.List;
+
 import org.elixirian.kommonlee.collect.Kollection;
 import org.elixirian.kommonlee.collect.ReadableList;
 import org.elixirian.kommonlee.collect.UnmodifiableIterator;
@@ -45,7 +47,7 @@ import org.elixirian.kommonlee.type.functional.Function1;
  *  /        \ /  /_/ _/  _  _  /  _  _  //  /_/ _/   __   //    /___/  _____/  _____/
  * /____/\____\/_____//__//_//_/__//_//_/ /_____//___/ /__//________/\_____/ \_____/
  * </pre>
- *
+ * 
  * <pre>
  *     ___  _____                                _____
  *    /   \/    /_________  ___ ____ __ ______  /    /   ______  ______
@@ -53,54 +55,57 @@ import org.elixirian.kommonlee.type.functional.Function1;
  *  /        \ /  _____/\    //   //   __   / /    /___/  _____/  _____/
  * /____/\____\\_____/   \__//___//___/ /__/ /________/\_____/ \_____/
  * </pre>
- *
+ * 
  * @author Lee, SeongHyun (Kevin)
  * @version 0.0.1 (2011-09-18)
  */
 public interface ImmutableList<E> extends ReadableList<E>, ImmutableCollection<E>
 {
-	@Override
-	UnmodifiableIterator<E> iterator();
+  @Override
+  UnmodifiableIterator<E> iterator();
 
-	@Override
-	int length();
+  @Override
+  int length();
 
-	@Override
-	boolean isEmpty();
+  @Override
+  boolean isEmpty();
 
-	@Override
-	boolean isNotEmpty();
+  @Override
+  boolean isNotEmpty();
 
-	@Override
-	boolean contains(Object element);
+  @Override
+  boolean contains(Object element);
 
-	@Override
-	boolean containsAll(Kollection<?> kollection);
+  @Override
+  boolean containsAll(Kollection<?> kollection);
 
-	@Override
-	ImmutableList<E> select(Condition1<? super E> condition);
+  @Override
+  ImmutableList<E> select(Condition1<? super E> condition);
 
-	@Override
-	<R> ImmutableList<R> map(Function1<? super E, R> function);
+  @Override
+  <R> ImmutableList<R> map(Function1<? super E, R> function);
 
-	@Override
-	<R> ImmutableList<R> mapSelectively(Condition1<? super E> condition, Function1<? super E, R> function);
+  @Override
+  <R> ImmutableList<R> mapSelectively(Condition1<? super E> condition, Function1<? super E, R> function);
 
-	@Override
-	int hashCode();
+  @Override
+  int hashCode();
 
-	@Override
-	boolean equals(Object element);
+  @Override
+  boolean equals(Object element);
 
-	@Override
-	E get(int index);
+  @Override
+  E get(int index);
 
-	@Override
-	int indexOf(E element);
+  @Override
+  int indexOf(E element);
 
-	@Override
-	int lastIndexOf(E element);
+  @Override
+  int lastIndexOf(E element);
 
-	@Override
-	ImmutableList<E> subList(int fromIndex, int toIndex);
+  @Override
+  ImmutableList<E> subList(int fromIndex, int toIndex);
+
+  @Override
+  List<E> convertTo();
 }
